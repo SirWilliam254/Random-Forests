5933d2812211d67a8ff8b3d9423de7b29d477cec
William Mburu <69644126+SirWilliam254@users.noreply.github.com>
2021-10-15 07:07:56 +0300

    Update RF.py

--- RF.py
+++ RF.py
@@ -1,1 +1,13 @@
+from sklearn.ensemble import RandomForestRegressor
 
+# Define the model. Set random_state to i
+rf_model = RandomForestRegressor(random_state = i)
+
+# fit your model
+rf_model.fit(train_X, train_y)
+
+# Calculate the mean absolute error of your Random Forest model on the validation data
+predicted = rf_model.predict(val_X)
+rf_val_mae = mean_absolute_error(val_y, predicted)
+
+print("Validation MAE for Random Forest Model: {}".format(rf_val_mae))
